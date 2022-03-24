4dec41a3e4b84d7d043b6d2be84f1deba3ad72ca
xgqfrms <xgqfrms@outlook.com>
2022-03-24 16:21:04 +0800

    Create arguments.js
    process.argv

---
+++ 000-xyz/arguments.js
@@ -0,0 +1,12 @@
+// function func (a, b) {
+//   console.log('a, b', a, b);
+//   console.log('arguments', arguments);
+//   for (let arg of arguments) {
+//       console.log('arg', arg);
+//   }
+// }
+
+// func(1,'a');
+
+console.log(process.argv);
+// console.log(process.env.arguments);
