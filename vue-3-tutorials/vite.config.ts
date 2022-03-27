630fafe4fd143fe9a4c4ea9ef036fa1b82c9f2dc
xgqfrms <xgqfrms@outlook.com>
2022-03-27 22:24:33 +0800

    feat: lock version

--- vue-3-tutorials/vite.config.ts
+++ vue-3-tutorials/vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // host: '0.0.0.0',
+    // host: true,
+    // port: 8888,
+  },
   plugins: [vue()]
 })
